d4f4c40b91cbc98dd101c00e83eb15539a550a61
InnerPeace080 <tranphuong.080@gmail.com>
2016-07-07 17:29:24 +0700

    fixup! suport RN-0.29

--- src/main/java/me/neo/react/StatusBarModule.java
+++ src/main/java/me/neo/react/StatusBarModule.java
@@ -1,6 +1,5 @@
 package me.neo.react;
 
-import android.app.Activity;
 import android.graphics.Color;
 import android.os.Build;
 import android.view.View;
@@ -15,10 +14,8 @@ import com.facebook.react.bridge.ReactMethod;
  * Created by Nishanth Shankar on 9/24/15.
  */
 public class StatusBarModule extends ReactContextBaseJavaModule {
-    Activity mActivity = null;
     public StatusBarModule(ReactApplicationContext reactContext) {
         super(reactContext);
-        mActivity = getCurrentActivity();
     }
 
     @Override
@@ -45,64 +42,72 @@ public class StatusBarModule extends ReactContextBaseJavaModule {
     }
 
     void setStatusColor(final int color){
-        mActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if (Build.VERSION.SDK_INT >= 21) {
-                    Window window = mActivity.getWindow();
-                    window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-                    window.setStatusBarColor(color);
+        if (getCurrentActivity()!=null) {
+            getCurrentActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (Build.VERSION.SDK_INT >= 21) {
+                        Window window = getCurrentActivity().getWindow();
+                        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+                        window.setStatusBarColor(color);
+
+                    }
 
                 }
-
-            }
-        });
+            });
+        }
 
     }
 
 
     @ReactMethod
     public void hideStatusBar(){
-        mActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                View decorView = mActivity.getWindow().getDecorView();
-                // Hide the status bar.
-                int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN ;
-                decorView.setSystemUiVisibility(uiOptions);
-            }
-        });
+        if (getCurrentActivity()!=null) {
+            getCurrentActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    View decorView = getCurrentActivity().getWindow().getDecorView();
+                    // Hide the status bar.
+                    int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
+                    decorView.setSystemUiVisibility(uiOptions);
+                }
+            });
+        }
     }
 
     @ReactMethod
     public void showStatusBar(){
-        mActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                if(Build.VERSION.SDK_INT < 16)
-                    return;
-                View decorView = mActivity.getWindow().getDecorView();
-                // Hide the status bar.
-                int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
-                decorView.setSystemUiVisibility(uiOptions);
-            }
-        });
+        if (getCurrentActivity()!=null) {
+            getCurrentActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (Build.VERSION.SDK_INT < 16)
+                        return;
+                    View decorView = getCurrentActivity().getWindow().getDecorView();
+                    // Hide the status bar.
+                    int uiOptions = View.SYSTEM_UI_FLAG_VISIBLE;
+                    decorView.setSystemUiVisibility(uiOptions);
+                }
+            });
+        }
     }
 
     @ReactMethod
     public void fullScreen(){
-        mActivity.runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-                View decorView = mActivity.getWindow().getDecorView();
-                if(Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
-                    decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
-                } else if(Build.VERSION.SDK_INT >= 19) {
-                    //for new api versions.
-                    int uiOptions =View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
-                    decorView.setSystemUiVisibility(uiOptions);
+        if (getCurrentActivity()!=null) {
+            getCurrentActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    View decorView = getCurrentActivity().getWindow().getDecorView();
+                    if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
+                        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+                    } else if (Build.VERSION.SDK_INT >= 19) {
+                        //for new api versions.
+                        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                        decorView.setSystemUiVisibility(uiOptions);
+                    }
                 }
-            }
-        });
+            });
+        }
     }
 }
