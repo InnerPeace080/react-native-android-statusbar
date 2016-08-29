1cc1e29513d710c80674af4371c399b78916b5c6
InnerPeace080 <tranphuong.080@gmail.com>
2016-08-29 10:08:34 +0700

    fix fullScreen android < 19

--- src/main/java/me/neo/react/StatusBarModule.java
+++ src/main/java/me/neo/react/StatusBarModule.java
@@ -100,7 +100,7 @@ public class StatusBarModule extends ReactContextBaseJavaModule {
                 public void run() {
                     View decorView = getCurrentActivity().getWindow().getDecorView();
                     if (Build.VERSION.SDK_INT > 11 && Build.VERSION.SDK_INT < 19) { // lower api
-                        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);
+                        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN| View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_FULLSCREEN);
                     } else if (Build.VERSION.SDK_INT >= 19) {
                         //for new api versions.
                         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
